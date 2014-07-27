3578b9a6af41684741841b174286d1a7355cd65a
Yos Riady <yosriady@gmail.com>
2014-07-27 14:51:24 +0800

    Refactor difficulty to use enum

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,10 +1,5 @@
 class Sheet < ActiveRecord::Base
-
-  # Difficulty integer: 0=> beginner, 1=> intermediate, 2=>advanced
-  DIFFICULTY = %w{ beginner intermediate advanced }
-  DIFFICULTY.each_with_index do |meth, index|
-    define_method("#{meth}?") { difficulty == index }
-  end
+  enum difficulty: %w{ beginner intermediate advanced }
 
   bitmask :instruments, :as => [:guitar, :piano, :bass, :mandolin, :banjo, :ukulele, :violin, :flute, :harmonica, :trombone, :trumpet, :clarinet, :saxophone, :others]
 end
