a6af16ab2d388858312d9b3f6f8647f48fe75f65
Yos Riady <yosriady@gmail.com>
2014-07-27 20:18:05 +0800

    Implement find_related

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,5 +1,30 @@
 class Sheet < ActiveRecord::Base
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :composers, :genres
+
   enum difficulty: %w{ beginner intermediate advanced }
 
   bitmask :instruments, :as => [:guitar, :piano, :bass, :mandolin, :banjo, :ukulele, :violin, :flute, :harmonica, :trombone, :trumpet, :clarinet, :saxophone, :others], :null => false
+
+  def find_related
+    sql = "
+    SELECT sheets.*, COUNT(tags.id) AS count
+    FROM sheets, tags, taggings
+    WHERE (sheets.id != #{id}
+           AND sheets.id = taggings.taggable_id
+           AND taggings.taggable_type = 'Sheet'
+           AND taggings.tag_id = tags.id
+           AND tags.name IN (#{joined_tags}))
+    GROUP BY sheets.id ORDER BY count DESC
+    "
+    Sheet.find_by_sql(sql)
+  end
+
+  def joined_tags
+    joined_tags = ""
+    tag_list.each do |tag|
+      joined_tags << "'#{tag}',"
+    end
+    return joined_tags[0..-2]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Sheet.create(title: "Another Guldove", description: "Guitar piece by Yasunori Mitsuda, from Chrono Cross.", pages: 2)
+Sheet.create(title: "Another Guldove", description: "Guitar piece by Yasunori Mitsuda, from Chrono Cross.", pages: 2, instruments: [:guitar], tag_list: [:summer])
+
+Sheet.create(title: "Another Marbule", description: "Guitar piece by Yasunori Mitsuda, from Chrono Cross.", pages: 2, instruments: [:piano], tag_list: [:summer, :memories])
+
+Sheet.create(title: "Another Termina", description: "Guitar piece by Yasunori Mitsuda, from Chrono Cross.", pages: 2, instruments: [:guitar], tag_list: [:summer, :memories, :beach])
