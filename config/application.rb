46f0bd82bbbce1f21ae1df8a41eb31acb1076692
Yos Riady <yosriady@gmail.com>
2014-07-27 20:20:01 +0800

    acts-as-taggable-on config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Sheethub
   class Application < Rails::Application
+
+    ActsAsTaggableOn.force_lowercase = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
