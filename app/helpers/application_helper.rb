8487708e561536fbbc83710b79b2c6128fc2910a
Yos Riady <yosriady@gmail.com>
2014-08-04 00:17:50 +0800

    Add flash messages partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,32 @@ module ApplicationHelper
     content_for :title, " | " + page_title.to_s if page_title
   end
 
+  def bootstrap_class_for flash_type
+    case flash_type.to_sym
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+
+  def glyphicon_for flash_type
+    case flash_type.to_sym
+      when :success
+        "ok"
+      when :error
+        "remove"
+      when :alert
+        "warning-sign"
+      else
+        "music"
+    end
+  end
+
 end
