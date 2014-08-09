b2bb47fa4bc64dc97c9330fbb94868cd8269454d
Yos Riady <yosriady@gmail.com>
2014-08-09 21:59:36 +0800

    Update title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def title(page_title)
-    content_for :title, " | " + page_title.to_s if page_title
+    content_for :title, page_title.to_s + " | " if page_title
   end
 
   def bootstrap_class_for flash_type
