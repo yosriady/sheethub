02e6d41fa5c0bf933d61eaa2cbfc8200d36a83b1
Yos Riady <yosriady@gmail.com>
2014-09-01 17:45:17 +0800

    Disallow users from accessing edit_user_registration_path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+
+  # GET /resource/edit
+  def edit
+    redirect_to root_path
+    # binding.pry
+    # Disallow users from editing their account details i.e. email, username
+  end
+
+
 end
