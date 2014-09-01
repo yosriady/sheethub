9ab35596c27eb87fffc494bb4adcf9dffe91e1cc
Yos Riady <yosriady@gmail.com>
2014-09-01 15:43:12 +0800

    Work on User profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
   match 'users/finish_registration' => 'finish_registrations#finish_registration', via: [:get, :patch], :as => :finish_registration
 
   resources :sheets do
