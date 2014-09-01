20a14b487df7c24e76fb5b71c4c8e57f92d87a63
Yos Riady <yosriady@gmail.com>
2014-09-01 11:01:47 +0800

    Add dependent destroy to has_many sheets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :sheets
+  has_many :sheets, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
