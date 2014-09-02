531c9ce1b229b9de43ef93ea0a040cc403bcf257
Yos Riady <yosriady@gmail.com>
2014-09-02 18:49:03 +0800

    Implement Edit Profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,8 +11,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/edit
   def edit
-    # Disallow users from editing their account details i.e. email, username
-    redirect_to profile_path(current_user.username)
+    # TODO: Edit Profile
+  end
+
+  def update
+    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+
+    # required for settings form to submit when password is left blank
+    if account_update_params[:password].blank?
+      account_update_params.delete("password")
+      account_update_params.delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(account_update_params)
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to profile_path(@user.username)
+    else
+      render "edit"
+    end
   end
 
   def finish_registration
