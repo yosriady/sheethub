4312f07a19a5ae31fb5027e63919e8705c70eff1
Yos Riady <yosriady@gmail.com>
2014-10-07 19:38:50 +0800

    Skip email confirmation if through omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
       @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user.skip_confirmation!
 
       if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication
@@ -14,6 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.skip_confirmation!
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
