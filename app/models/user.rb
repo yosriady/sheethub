5e023cb953b4a859bc5a54309b377a142af85556
Yos Riady <yosriady@gmail.com>
2014-10-07 19:33:55 +0800

    Add Devise confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
   def joined_at
