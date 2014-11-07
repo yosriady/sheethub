32992983b613143c8c46fe66947eff2f7d5c7617
Yos Riady <yosriady@gmail.com>
2014-11-07 15:52:01 +0800

    Implement checkout modal

--- app/assets/javascripts/paypal-checkout.js
+++ app/assets/javascripts/paypal-checkout.js
@@ -1,9 +1,12 @@
-// $(document).on("page:change", function() {
-//     $("#paypal-checkout-btn").one('click',function(e){
-//         $(this).on('click',function(ev){
-//           ev.preventDefault();
-//         });
-//         // Pop open modal
-//         $('#paypalProgressModal').modal('show');
-//     });
-// });
+$(document).on("page:change", function() {
+    $("#paypal-checkout-btn").one('click',function(e){
+        $("#paypal-checkout-btn button").addClass("disabled");
+        $("#paypal-checkout-btn button").text("Please wait while we redirect you to Paypal...")
+        $('#modal-close-btn').remove();
+        $('#loading-content').removeClass("hidden");
+        $(this).on('click',function(ev){
+          ev.preventDefault();
+          return false;
+        });
+    });
+});
