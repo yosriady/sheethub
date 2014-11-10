8e2fa5e17585b9d64bd3b4d5e0f2096d4298f9fd
Yos Riady <yosriady@gmail.com>
2014-11-10 11:51:34 +0800

    Allow users to change username

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ActiveRecord::Base
   validates_email_format_of :email, message: 'You have an invalid email address'
   validates_email_format_of :paypal_email, message: 'You have an invalid paypal account email address', if: :has_paypal_email?
   validate :validate_number_of_uploaded_sheets
-  before_save :downcase_username
   has_many :sheets, dependent: :destroy
   acts_as_voter
 
@@ -134,10 +133,6 @@ class User < ActiveRecord::Base
   end
 
   private
-    def downcase_username
-      username = username.downcase unless username.blank?
-    end
-
     def validate_number_of_uploaded_sheets
       errors.add(:sheets, "You have exceeded the number of sheets you can upload. Free users have 30, Premium can have up to 250. Upgrade your membership to continue publishing on SheetHub. ") if sheets.size > membership_sheet_quantity
     end
