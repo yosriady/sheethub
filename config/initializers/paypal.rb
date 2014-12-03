759625ab86c87d5994fe2aad6ee1caecb5b3a2ae
Yos Riady <yosriady@gmail.com>
2014-12-04 02:36:17 +0800

    Refactor paypal.rb config to secrets.yml, minor refactors

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,13 +1,7 @@
-PAYPAL_MODE = "sandbox"
-PAYPAL_CLASSIC_APP_ID = "APP-80W284485P519543T" #SANDBOX
-PAYPAL_USERNAME = "yosriady-facilitator_api1.gmail.com"
-PAYPAL_PASSWORD = "H739SP4UDQSHEGAW"
-PAYPAL_SIGNATURE = "A6AFKLF9K9Odzd81iNIOAJrlric4AKKVz.DGUTuNFWqyRFUGYkyp5-Ya"
-
 PayPal::SDK.logger = Rails.logger
 PayPal::SDK.configure(
-  :mode      => PAYPAL_MODE,  # Set "live" for production
-  :app_id    => PAYPAL_CLASSIC_APP_ID,
-  :username  => PAYPAL_USERNAME,
-  :password  => PAYPAL_PASSWORD,
-  :signature => PAYPAL_SIGNATURE)
+  :mode      => Rails.application.secrets.paypal_mode,
+  :app_id    => Rails.application.secrets.paypal_classic_app_id,
+  :username  => Rails.application.secrets.paypal_username,
+  :password  => Rails.application.secrets.paypal_password,
+  :signature => Rails.application.secrets.paypal_signature)
