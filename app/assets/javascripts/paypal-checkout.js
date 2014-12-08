d0aa802439680209a66bdb9ed2297f7810eab957
Yos Riady <yosriady@gmail.com>
2014-12-09 00:35:05 +0800

    Replace i with em

--- app/assets/javascripts/paypal-checkout.js
+++ app/assets/javascripts/paypal-checkout.js
@@ -1,7 +1,7 @@
 $(document).on("page:change", function() {
     $("#paypal-checkout-btn").one('click',function(e){
         $("#paypal-checkout-btn button").addClass("disabled");
-        $("#paypal-checkout-btn button").html("<i class='fa fa-spinner fa-spin'></i> Please wait while we redirect you to Paypal...")
+        $("#paypal-checkout-btn button").html("<em class='fa fa-spinner fa-spin'></em> Please wait while we redirect you to Paypal...")
         $('#modal-close-btn').remove();
         $(this).on('click',function(ev){
           ev.preventDefault();
