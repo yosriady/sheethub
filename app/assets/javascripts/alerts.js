f0d712b6fe2ff912166997662f5b14c155506bcc
Yos Riady <yosriady@gmail.com>
2014-12-14 14:23:19 +0800

    Auto hide alerts

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,3 +1,8 @@
 $(document).on("page:change", function() {
     $(".alert").addClass("in");
+    window.setTimeout(function() {
+        $(".alert").fadeTo(300, 0).slideUp(300, function(){
+            $(this).remove();
+        });
+    }, 3000);
 });
