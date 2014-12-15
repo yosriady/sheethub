55e6cc0b2cfa54d0007b7ed54478da6fb57071e1
Yos Riady <yosriady@gmail.com>
2014-12-16 01:39:22 +0800

    Update session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_sheethub_session', domain: ".sheethub.co"
+  Rails.application.config.session_store :cookie_store, key: '_sheethub_prod_session', domain: ".sheethub.co"
 else
-  Rails.application.config.session_store :cookie_store, key: '_sheethub_session'
+  Rails.application.config.session_store :cookie_store, key: '_sheethub_dev_session', domain: "lvh.me", tld_length: 2
 end
