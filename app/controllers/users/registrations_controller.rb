957dca086f6e062ef0cc1a31fbe9baec06378e5c
Yos Riady <yosriady@gmail.com>
2014-12-16 20:57:06 +0800

    Enhancements

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def library
     track('View library')
-    @purchases = current_user.purchased_orders.page(params[:page])
+    @purchases = current_user.purchased_orders.includes(sheet: [:user, :assets]).page(params[:page])
   end
 
   def sales
