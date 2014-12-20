48d69d9416b3623dd1fec3b001c19cf49e5f4db7
Yos Riady <yosriady@gmail.com>
2014-12-20 16:27:24 +0800

    Rubocop refactor config initializers

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
 AWS.config({
-  :access_key_id => Rails.application.secrets.s3_access_key_id,
-  :secret_access_key => Rails.application.secrets.s3_secret_access_key,
-  :region => Rails.application.secrets.s3_region
+  access_key_id: Rails.application.secrets.s3_access_key_id,
+  secret_access_key: Rails.application.secrets.s3_secret_access_key,
+  region: Rails.application.secrets.s3_region
 })
--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,7 +1,8 @@
 PayPal::SDK.logger = Rails.logger
 PayPal::SDK.configure(
-  :mode      => Rails.application.secrets.paypal_mode,
-  :app_id    => Rails.application.secrets.paypal_classic_app_id,
-  :username  => Rails.application.secrets.paypal_username,
-  :password  => Rails.application.secrets.paypal_password,
-  :signature => Rails.application.secrets.paypal_signature)
+  mode: Rails.application.secrets.paypal_mode,
+  app_id: Rails.application.secrets.paypal_classic_app_id,
+  username: Rails.application.secrets.paypal_username,
+  password: Rails.application.secrets.paypal_password,
+  signature: Rails.application.secrets.paypal_signature
+)
