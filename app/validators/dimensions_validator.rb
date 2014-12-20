77d78c0d6bbd7790dfef05a34f682495fb166671
Yos Riady <yosriady@gmail.com>
2014-12-20 20:46:16 +0700

    Fix dimensions validator

--- app/validators/dimensions_validator.rb
+++ app/validators/dimensions_validator.rb
@@ -1,12 +1,12 @@
 class DimensionsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return unless record.send('#{attribute}?'.to_sym) && value.queued_for_write[:original].present?
+    return unless record.send("#{attribute}?".to_sym) && value.queued_for_write[:original].present?
     image = value.queued_for_write[:original]
     dimensions = Paperclip::Geometry.from_file(image.path)
     width = options[:width]
     height = options[:height]
 
-    record.errors[attribute] << 'Width must be #{width}px' unless dimensions.width <= width
-    record.errors[attribute] << 'Height must be #{height}px' unless dimensions.height <= height
+    record.errors[attribute] << "Width must be #{width}px" unless dimensions.width <= width
+    record.errors[attribute] << "Height must be #{height}px" unless dimensions.height <= height
   end
 end
