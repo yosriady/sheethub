3932e95c79d4312de53f09ae1531f7e0c4bc7197
Yos Riady <yosriady@gmail.com>
2014-12-20 15:32:25 +0800

    Fix paypal-checkout.js

--- app/assets/javascripts/paypal-checkout.js
+++ app/assets/javascripts/paypal-checkout.js
@@ -1,7 +1,7 @@
 $(document).on("page:change", function() {
     $("#paypal-checkout-btn").one('click',function(e){
-        $("#paypal-checkout-btn button").addClass("disabled");
-        $("#paypal-checkout-btn button").html("<em class='fa fa-spinner fa-spin'></em> Please wait while we redirect you to Paypal...")
+        $("#paypal-checkout-btn").addClass("disabled");
+        $("#paypal-checkout-btn").html("<em class='fa fa-spinner fa-spin'></em> Please wait while we redirect you to Paypal...")
         $('#modal-close-btn').remove();
         $(this).on('click',function(ev){
           ev.preventDefault();
