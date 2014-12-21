253984ccc79672df0cee82fb54d13ba5fc412d95
Yos Riady <yosriady@gmail.com>
2014-12-21 21:56:00 +0700

    Minor enhancement

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,27 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      flash[:error] = @user.errors.empty? ? "Error" : @user.errors.full_messages.to_sentence
+      binding.pry
+      session['devise.google_data'] = request.env['omniauth.auth']
+      flash[:error] = @user.errors.empty? ? 'Error' : @user.errors.full_messages.to_sentence
       redirect_to new_user_registration_url
     end
   end
 
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except('extra')
-      flash[:error] = @user.errors.empty? ? "Error" : @user.errors.full_messages.to_sentence
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except('extra')
+      flash[:error] = @user.errors.empty? ? 'Error' : @user.errors.full_messages.to_sentence
       redirect_to new_user_registration_url
     end
   end
