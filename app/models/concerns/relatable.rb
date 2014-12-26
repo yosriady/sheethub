69989b83ef6c471d9c92aa0579cab4d636516e52
Yos Riady <yosriady@gmail.com>
2014-12-27 00:53:03 +0700

    Re-implement caching for relatable module methods

--- app/models/concerns/relatable.rb
+++ app/models/concerns/relatable.rb
@@ -6,35 +6,26 @@ module Relatable
     DEFAULT_NUM_RELATED_RESULTS = 3
   end
 
+  def cache_key_for_related_sheets
+    "related_sheets_to_sheet_#{id}_#{updated_at}"
+  end
+
   def related_sheets(num_results = DEFAULT_NUM_RELATED_RESULTS)
     return [] if joined_tags.empty?
-    sql = "
-    SELECT sheets.*, COUNT(tags.id) AS count
-    FROM sheets, tags, taggings
-    WHERE (sheets.id != #{id}
-           AND sheets.id = taggings.taggable_id
-           AND taggings.taggable_type = 'Sheet'
-           AND taggings.tag_id = tags.id
-           AND tags.name IN (#{joined_tags}))
-    GROUP BY sheets.id ORDER BY count DESC
-    LIMIT #{num_results}
-    "
-    # Rails.cache.fetch('related_sheets_to_#{self}', expires_in: 1.day) do
-      Sheet.find_by_sql(sql)
-    # end
+    Rails.cache.fetch(cache_key_for_related_sheets, expires_in: 1.week) do
+      Sheet.tagged_with(joined_tags, any: true).includes(:sources, :composers, :genres).limit(num_results)
+    end
+  end
+
+  def cache_key_for_related_tags
+    "related_tags_for_sheet_#{id}-#{updated_at}"
   end
 
   def related_tags
-    # Rails.cache.fetch('related_tags_to_#{self}', expires_in: 1.day) do
+    Rails.cache.fetch(cache_key_for_related_tags, expires_in: 1.week) do
       related_tags = Set.new
-      related_sheets_via_tags.find_each { |sheet| related_tags.merge sheet.tag_objects }
+      related_sheets.find_each { |sheet| related_tags.merge sheet.tag_objects }
       related_tags.to_a
-    # end
-  end
-
-  def related_sheets_via_tags
-    # Rails.cache.fetch('related_sheets_via_tags_to_#{self}', expires_in: 1.day) do
-      Sheet.tagged_with(joined_tags, any: true).includes(:sources, :composers, :genres).limit(5)
-    # end
+    end
   end
 end
