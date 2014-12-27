325780c022b25228fa8db2df3223d1699c6a9979
Yos Riady <yosriady@gmail.com>
2014-12-28 00:28:34 +0700

    Update email styles

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -11,7 +11,7 @@ class ContactForm < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      subject: "SheetHub Support: #{subject}",
+      subject: "New SheetHub Support Ticket: #{subject}",
       to: "yosriady@gmail.com",
       from: %("#{name}" <#{email}>)
     }
