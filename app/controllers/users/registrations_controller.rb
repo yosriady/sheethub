c29b1fd3704cab0e4506adb36c08ea849286d52b
Yos Riady <yosriady@gmail.com>
2014-12-31 21:38:58 +0700

    Update sales, refactor paginated partials

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,9 +39,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def sales
     track('View sales')
+    @all_sales = current_user.sales.page(params[:page])
     @aggregated_sales = current_user.aggregated_sales
     @sales_past_month = current_user.sales_past_month
-    @all_sales = current_user.sales.page(params[:page])
   end
 
   def trash
