903378348861a801fc6973d32d046888dab7d387
Yos Riady <yosriady@gmail.com>
2015-02-18 18:40:44 +0800

    CSS Refactors, minor redesign

---
+++ app/assets/javascripts/vextab-editor.js
@@ -0,0 +1,6 @@
+$(document).on("page:change", function() {
+    // var editor = $("#vextab-editor textarea.editor")[0];
+    // if (editor){
+    //     var myCodeMirror = CodeMirror.fromTextArea(editor);
+    // }
+});
