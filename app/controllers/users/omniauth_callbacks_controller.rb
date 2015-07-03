6e0ef6a1fcaa2a5c32756ef5e20fbecf5afe8cb9
Yos Riady <yosriady@gmail.com>
2015-07-03 20:17:23 +0800

    Omniauth hotfix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,29 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  [:google_oauth2, :facebook].each do |provider|
-    define_method(:provider) do
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider.titleize) if is_navigational_format?
-      else
-        flash[:error] = @user.errors.empty? ? 'Error' : @user.errors.full_messages.to_sentence
-        redirect_to new_user_registration_url
-      end
+
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    else
+      # session['devise.google_data'] = request.env['omniauth.auth']
+      flash[:error] = @user.errors.empty? ? 'Error' : @user.errors.full_messages.to_sentence
+      redirect_to new_user_registration_url
     end
   end
+
+  def facebook
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    else
+      # session['devise.facebook_data'] = request.env['omniauth.auth'].except('extra')
+      flash[:error] = @user.errors.empty? ? 'Error' : @user.errors.full_messages.to_sentence
+      redirect_to new_user_registration_url
+    end
+  end
+
 end
